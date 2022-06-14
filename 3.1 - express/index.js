0c0437afebda35e34e130459328f60fe5c035a7d
juliamdeiros <juliarmedeiros@gmail.com>
2022-06-14 15:24:33 -0300

    parametros (3.13)

--- 3.1 - express/index.js
+++ 3.1 - express/index.js
@@ -1,16 +1,35 @@
 const express = require("express"); //importando express
 const app = express(); //iniciando express
 
+//req dados enviados pelo usuario
+//res resposta para o usuario
 app.get("/", function(req, res){ //rota
-    res.send("Bem vindo!");
+    res.send("<h1>Bem vindo!</h1>");
 });
 
-app.get("/blog", function(req, res){
-    res.send("Bem vindo ao blog!");
+app.get("/blog/:artigo?", function(req, res){
+
+    var artigo = req.params.artigo;
+    if(artigo){
+        res.send("Artigo: " + artigo);
+    } else {
+        res.send("Bem vindo ao blog!");
+    }
 });
 
 app.get("/youtube", function(req, res){
-    res.send("Bem vindo ao canal!");
+    var canal = req.query["canal"];
+    if(canal){
+       res.send(canal); 
+    } else {
+        res.send("Nenhum canal fornecido"); 
+    }
+    
+});
+
+app.get("/ola/:nome", function(req, res){
+    var nome = req.params.nome;
+    res.send("<h1>Oi " + nome + "</h1>");
 });
 
 app.listen(4000, function(erro){ //inicia servidor
