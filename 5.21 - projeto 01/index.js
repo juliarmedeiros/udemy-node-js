2b9c28ffbfe73da4f8b1ebec6356260036d74487
juliamdeiros <juliarmedeiros@gmail.com>
2022-06-14 16:51:45 -0300

    arquivos estaticos (5.27)

--- 5.21 - projeto 01/index.js
+++ 5.21 - projeto 01/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs'); //express usando ejs como view engine
+app.use(express.static('public'));
 
 app.get("/:nome?/:lang?", (req, res) =>{
     var nome = req.params.nome;
